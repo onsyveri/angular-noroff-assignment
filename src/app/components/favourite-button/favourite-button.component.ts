5fa55e4118968c00058dba72138c8e62d3d1549a
Carl Markus Malde <carl.markus.malde@no.experis.com>
2022-09-28 10:32:20 +0200

    added favicons and functionality to catch pokemons

--- src/app/components/favourite-button/favourite-button.component.ts
+++ src/app/components/favourite-button/favourite-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-favourite-button',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavouriteButtonComponent implements OnInit {
 
+  @Input() pokemonName: string =""; 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onFavouriteClick(): void {
+    //Add pokemon to favourites/inventory 
+    alert("You cought: " + this.pokemonName)
   }
 
 }
